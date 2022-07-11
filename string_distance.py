b0403ea7e307bbcf83c41e260b686472625c21b2
Avi Semler <89142849+avisemler@users.noreply.github.com>
2022-07-11 16:45:02 +0100

    Add non-recursive Levenshtein for longer seqs

--- string_distance.py
+++ string_distance.py
@@ -4,17 +4,28 @@ import functools
 def distance (seq1, seq2):
     #levenshtein distance
 
-    #base cases
-    if len(seq1) == 0:
-        return len(seq2)
-    elif len(seq2) == 0:
-        return len(seq1)
+    m = len(seq1)
+    n = len(seq2)
+    matrix = np.zeros((m + 1, n + 1))
 
-    if seq1[0] == seq2[0]:
-        return distance(seq1[1:], seq2[1:])
-    else:
-        return 1 + min((
-            distance(seq1[1:], seq2),
-            distance(seq1, seq2[1:]),
-            distance(seq1[1:], seq2[1:])
-        ))
+    for i in range(m):
+        #The distance between the a prefix and the empty string is the length
+        matrix[i + 1, 0] = i + 1
+
+    for j in range(n):
+        matrix[0, j + 1] = j + 1
+
+    for j in range(1, n + 1):
+        for i in range(1, m + 1):
+            print(i,j)
+            if seq1[i - 1] == seq2[j - 1]:
+                cost = 0
+            else:
+                cost = 1
+            matrix[i, j] = min([
+                matrix[i - 1, j] + 1,
+                matrix[i, j - 1] + 1,
+                matrix[i - 1, j - 1] + cost,
+            ])
+            
+    return matrix[m, n]
